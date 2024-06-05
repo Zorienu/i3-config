9e69c1b2bd7041e17060f7835a766f04caa0d1d8
Jasyd <jasydavid45@gmail.com>
2024-06-04 19:57:00 -0500

    set second monitor frequency in setup shell script

--- setupI3.sh
+++ setupI3.sh
@@ -2,7 +2,7 @@ laptopMonitor=eDP-1
 hdmi=HDMI-1
 
 if xrandr | grep "$hdmi connected"; then
-  xrandr --output $hdmi --mode 1920x1080 --right-of $laptopMonitor
+  xrandr --output $hdmi --mode 1920x1080 --refresh 144.01 --right-of $laptopMonitor
 else
   xrandr --output $hdmi --off
 fi
