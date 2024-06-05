1e76307f49262b95fab3f48a83e03dc272389ce7
Jasyd <jasydavid45@gmail.com>
2024-06-05 14:52:00 -0500

    configure the keyboard layout in the sh script

--- setupI3.sh
+++ setupI3.sh
@@ -5,9 +5,14 @@ xrandr --output $laptopMonitor --scale 0.8
 
 if xrandr | grep "$hdmi connected"; then
   xrandr --output $hdmi --mode 1920x1080 --refresh 144.01 --right-of $laptopMonitor --scale 1
+  # Configure to use the external keyboard layout
+  setxkbmap -layout es,es
 else
   xrandr --output $hdmi --off
+  # Configure to use the laptop keyboard layout
+  setxkbmap -layout us,us
 fi
+
 
 
 i3 restart
