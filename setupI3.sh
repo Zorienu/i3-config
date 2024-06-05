f3e2e11c77b203790c9cc6c7728a4739cc70e023
Jasyd <jasydavid45@gmail.com>
2024-06-04 21:45:35 -0500

    set scale factor for laptop monitor

--- setupI3.sh
+++ setupI3.sh
@@ -1,10 +1,13 @@
 laptopMonitor=eDP-1
 hdmi=HDMI-1
 
+xrandr --output $laptopMonitor --scale 0.8 
+
 if xrandr | grep "$hdmi connected"; then
-  xrandr --output $hdmi --mode 1920x1080 --refresh 144.01 --right-of $laptopMonitor
+  xrandr --output $hdmi --mode 1920x1080 --refresh 144.01 --right-of $laptopMonitor --scale 1
 else
   xrandr --output $hdmi --off
 fi
+
 
 i3 restart
