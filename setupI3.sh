6058b5ad9ef5ad66ddd81452457f8a34c546c640
Jasyd <jasydavid45@gmail.com>
2024-06-06 11:40:10 -0500

    fix cursor flickering in external monitor

--- setupI3.sh
+++ setupI3.sh
@@ -4,7 +4,8 @@ hdmi=HDMI-1
 xrandr --output $laptopMonitor --scale 0.8 
 
 if xrandr | grep "$hdmi connected"; then
-  xrandr --output $hdmi --mode 1920x1080 --refresh 144.01 --right-of $laptopMonitor --scale 1
+  # https://askubuntu.com/questions/450655/mouse-flickering-on-one-of-my-two-screens
+  xrandr --output $hdmi --mode 1920x1080 --refresh 144.01 --right-of $laptopMonitor --scale 0.9999x0.9999
   # Configure to use the external keyboard layout
   setxkbmap -layout es,es
 else
